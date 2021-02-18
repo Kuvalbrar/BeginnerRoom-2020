3838143ac3a79bd75b973c2edc017ec77cc30b6e
Kuvalbrar <77647838+Kuvalbrar@users.noreply.github.com>
2021-02-18 15:30:26 -0500

    Delete meme_text_kuval.py

--- meme_text_kuval.py
+++
@@ -1,9 +0,0 @@
-meme = input("input word you want to memefy:")
-
-meme_lower = meme.lower()
-meme_uppercase = meme.upper()
-
-here = meme_lower[0] + meme_uppercase[1] + meme_lower[2] + meme_uppercase[3]
-
-print(here)
-
